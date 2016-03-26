c1991ef75b4c2097095e8990ac1abef962fd2730
John Slota <john.slota@gmail.com>
2016-03-26 12:53:34 -0600

    first commit

---
+++ function-object.js
@@ -0,0 +1,5 @@
+function math(one, two, three) {
+  result = (one + (two * three))
+  console.log(result)
+}
+math(53, 61, 67)
---
+++ rounding-numbers.js
@@ -0,0 +1,3 @@
+var roundUp = 1.5
+var solution = Math.round(roundUp)
+console.log(solution)
